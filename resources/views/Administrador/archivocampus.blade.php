0ae57f1b104aa852db70ed048f116620e776897e
Gustavo Garcia <sindromedemc@hotmail.com>
2015-06-05 16:36:47 -0400

    Commit inicial

---
+++ resources/views/Administrador/archivocampus.blade.php
@@ -0,0 +1,20 @@
+@extends('Layout.layoutadmin')
+
+@section('contenido')
+        <!-- Page Content -->
+        <div id="page-wrapper" style="min-height: 586px;">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">Archivar Campus</h1>
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+                <!-- /.row -->
+            </div>
+            
+        </div>        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+    @stop
---
+++ resources/views/Administrador/index.blade.php
@@ -0,0 +1,39 @@
+@extends('Layout.layoutadmin')
+
+@section('contenido')
+
+        <!-- Page Content -->
+        <div id="page-wrapper" style="min-height: 586px;">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">Bienvenido</h1>
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <div class="row" style="background-color: rgb(15, 73, 163)">
+                <div class="row">
+                    <div class="page-header">
+                        
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <div class="row">
+                <div class="row">
+                    <div class="page-header">
+                        <img src="images/ss.jpg" alt=""/-->
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+                <!-- /.row -->
+            </div>
+        </div>        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    @stop
